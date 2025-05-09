06c026162cb50bcdfdfe5440c401ca99fad48cf8
tomxi1997 <78306449+tomxi1997@users.noreply.github.com>
2025-05-09 10:37:48 +0800

    Update repack.sh

--- repack.sh
+++ repack.sh
@@ -8,15 +8,16 @@ cd $DIRECTORY
 # Check if the directory exists
 if [ -d "$DIRECTORY" ]; then
     # Loop through each file in the directory
-    for FILE in "$DIRECTORY"/*; do
+    for FILE in "$DIRECTORY"/*.deb; do
         # Check if it's a file (not a directory)
             # Print the file name
             echo "$(basename "$FILE")"
-            tar -xf $(basename "$FILE")
+            #tar -xf $(basename "$FILE")
+            dpkg -x "$FILE"
             echo "解压'$(basename "$FILE")"
             echo "---------------------------------------"
         
     done
 
 
-tar -czvf data.tar.xz data
+
